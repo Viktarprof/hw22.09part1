00ca104b5bd7b2bbd3b17be37697dbe6ba37e41f
Viktarprof <113298552+Viktarprof@users.noreply.github.com>
2022-09-29 12:57:19 +0200

    Add files via upload

---
+++ Home_work22_09/Home_work22_09First level/src/main/java/main.java
@@ -0,0 +1,106 @@
+public class main {
+    public static void main(String[] args) {
+
+        int aq = 333;
+        int kol_soten = 100;
+        int kol_des = 10;
+        int kol_edenic = 1;
+        int result7 = aq / kol_soten;
+        int result8 = aq / kol_des;
+        int result9 = aq / kol_edenic;
+
+        System.out.println(result7);
+        System.out.println(result8);
+        System.out.println(result9);
+
+
+
+
+
+
+
+        char a ='G';
+        System.out.println(a);
+
+        int b = 80;
+        int c = 9;
+        Integer x = b + c;
+        System.out.println(x);
+
+        byte d = 8;
+        byte i = 2;
+        int result = d / i;
+        System.out.println(result);
+
+        short f = 7;
+        short g = 8;
+        int result1 = f * g;
+        System.out.println(result1);
+
+        float yq = 336067396;
+        float uq = 71000000;
+        float result2 = yq / uq;
+        System.out.println(result2);
+        
+        //прошу оставить комментарий по float
+        float y = 4.7333436f;
+        float u = 1;
+        float result22 = y / u;
+        System.out.println(result22);
+
+        //прошу оставить комментарий по float
+
+        double m = 4.355453532;
+        double n = 10;
+        double v = m % n;
+        System.out.println(v);
+
+        //прошу оставить комментарий по double
+
+        long s = 12121;
+        long z = 0;
+        long t = s + z;
+        System.out.println(t);
+
+
+        //char: G
+        //integer: 89
+        //byte: 4
+        //short: 56
+        //float: 4.7333436
+        //double: 4.355453532
+        //long: 12121
+
+        char one ='G';
+        System.out.println(one);
+
+        int two = 89;
+        Integer p = two;
+        System.out.println(two);
+
+        byte three = 4;
+        System.out.println(three);
+
+        short four = 56;
+        System.out.println(four);
+
+        float five = 4.7333436f;
+        System.out.println(five);
+
+        double six = 4.355453532;
+        System.out.println(six);
+
+        long seven = 12121;
+        System.out.println(seven);
+
+
+
+        //char: G
+        //integer: 89
+        //byte: 4
+        //short: 56
+        //float: 4.7333436
+        //double: 4.355453532
+        //long: 12121
+    }
+}
---
+++ Home_work22_09/Home_work22_09SecondLevel/Home_work22_09SecondLevel/src/main/java/main.java
@@ -0,0 +1,28 @@
+public class main {
+    public static void main(String[] args) {
+        int aa = 333;
+        int kol_soten = 100;
+        int kol_des = 10;
+        int kol_edenic = 1;
+        int result1 = aa / kol_soten;
+        int result2 = aa / kol_des;
+        int result3 = aa / kol_edenic;
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+
+
+        int a = 333;
+        int kol_sot = 100;
+        int kol_desiat = 10;
+        int kol_edeni = 1;
+
+        System.out.println(a / kol_sot);
+        System.out.println(a / kol_desiat);
+        System.out.println(a / kol_edeni);
+
+
+        //System.out.println(a: " + p.name + "\nage: " + p.age);
+    }
+}
